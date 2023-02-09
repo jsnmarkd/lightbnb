747679684f16422db5ca57dcd76721cbf68692ec
Jason Divinagracia <jmarkd04@gmail.com>
2023-02-09 13:44:32 -0500

    Refactored user reservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -18,7 +18,7 @@ const pool = new Pool({
  */
 const getUserWithEmail = function (email) {
   return pool
-    .query(`SELECT * FROM users WHERE email = $1`,[email])
+    .query(`SELECT * FROM users WHERE email = $1`, [email])
     .then((result) => {
       return result.rows[0];
     })
@@ -52,12 +52,15 @@ exports.getUserWithId = getUserWithId;
  */
 const addUser = function (user) {
   return pool
-    .query(`
+    .query(
+      `
       INSERT INTO 
       users (name, email, password) 
       VALUES ($1, $2, $3)
       RETURNING *;
-      `, [user.name, user.email, user.password])
+      `,
+      [user.name, user.email, user.password]
+    )
     .then((result) => {
       return result.rows[0];
     })
@@ -75,7 +78,34 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+      `
+      SELECT 
+        reservations.*, 
+        properties.title,
+        properties.thumbnail_photo_url,
+        properties.cost_per_night,
+        properties.parking_spaces,
+        properties.number_of_bathrooms,
+        properties.number_of_bedrooms,
+        avg(rating) as average_rating
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE reservations.guest_id = $1
+      GROUP BY properties.id, reservations.id
+      ORDER BY reservations.start_date
+      LIMIT $2;
+      `,
+      [guest_id, limit]
+    )
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
